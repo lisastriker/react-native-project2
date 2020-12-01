782a38aab7230e5cb9e3cc0795be6155b1db1ce8
lisastriker <lisastriker@gmail.com>
2020-12-01 12:08:08 +0800

    put in code

--- App.js
+++ App.js
@@ -2,13 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(){
+  let [count,setCount] = useState(0)
+  function increment(){ 
+    setCount(count + 1)
+  }
+
+  function renderEncouragingText(){
+    if (count == 10){
+      return "Keep Going"
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.text}>{count}</Text>
+        <TouchableOpacity style={styles.button} onPress={increment}>
+          <Text style={styles.buttonText}>Click me</Text>
+        </TouchableOpacity>
+          <Text style={styles.encouragingText}>{renderEncouragingText()}</Text>
+        <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +32,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'yellow',
   },
-});
+  text:{
+    textAlign:"center",
+    fontSize:40,
+    color:'red',
+    fontWeight:'bold',
+
+  },
+  encouragingText:{
+    marginTop:50,
+  },
+  button:{
+    backgroundColor:'red',
+  },
+  buttonText: {
+    padding:10,
+    color:'white',
+  }
+})
